2056ca4d6958e9c185d7e16ddf801a7bcbb6e09c
Bohdan Miskov <bohdanmiskov07@gmail.com>
2024-12-26 12:14:42 +0200

    Add general variables for calculations

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,7 @@
 'use strict';
-function makeTransaction (quantity, pricePerDroid){
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
